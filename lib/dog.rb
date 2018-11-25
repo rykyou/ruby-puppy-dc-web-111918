c57144a305d9cbd24ed6b23808ce63e635eb0f9d
Robin Kim <rkim1214@gmail.com>
2018-11-25 20:59:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all.uniq.each do |dog_name|
+      puts "#{dog_name.name}"
+    end
+  end 
+  
+  def self.clear_all 
+    @@all.clear 
+  end 
+  
+end 
